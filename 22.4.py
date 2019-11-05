24e8997e45a1ea92d815c69bf7abecbe049f9a9d
kami-sa <kamisa1999@yandex.ru>
2019-11-05 20:32:03 +0300

    Task 22.4 and 23.1

---
+++ 22.4.py
@@ -0,0 +1,27 @@
+import math
+
+
+def solve(*coefficients):
+    x = []
+    if len(coefficients) == 3:
+        a = coefficients[0]
+        b = coefficients[1]
+        c = coefficients[2]
+        d = (b ** 2) - 4 * a * c
+        if d >= 0:
+            x.append((-b + math.sqrt(d)) / (2 * a))
+            x.append((-b - math.sqrt(d)) / (2 * a))
+        else:
+            x.append('Нет действительных решений')
+    elif len(coefficients) == 2:
+        b = coefficients[0]
+        c = coefficients[1]
+        x.append(-c / b)
+    else:
+        x.append('None')
+    return x
+
+
+# print(solve(2,1))
+# print(solve(1, 2, 1))
+# print(solve(2,-6,0))
