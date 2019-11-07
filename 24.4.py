6c46919c4f54786c4c7356586454cf7234515061
kami-sa <kamisa1999@yandex.ru>
2019-11-07 18:33:42 +0300

    24.4 add

--- 24.4.py
+++ 24.4.py
@@ -1,4 +1,15 @@
 n = int(input())
 words = [input().lower().strip() for i in range(n)]
-words_set = set(set(words[i]) for i in range(n))
-for i in range(len(words_set)):
+sort_words = []
+for word in words:
+    sort_words.append(sorted(word))
+set_w = set()
+for i in range(len(words)):
+    if words[i] not in set_w:
+        print(words[i], end=' ')
+        set_w.add(words[i])
+        for j in range(i+1, len(words)):
+            if sort_words[i] == sort_words[j]:
+                print(words[j], end=' ')
+                set_w.add(words[j])
+        print()
