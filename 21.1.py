df5871396e3e6e4ed86ceba43c5f68f89082a9f6
kami-sa <kamisa1999@yandex.ru>
2019-11-07 12:39:25 +0300

    21.1 upd

--- 21.1.py
+++ 21.1.py
@@ -1,10 +1,8 @@
 def from_string_to_list(string, container):
     string = string[::-1]
     string = string.split()
-    i = 0;
     while len(string) != 0:
         container.append(string.pop())
-        i += 1
     return container
 
 
