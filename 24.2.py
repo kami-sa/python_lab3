54bf25a6704294b1ff71e273cf195a8fa124fe78
kami-sa <kamisa1999@yandex.ru>
2019-11-07 18:03:40 +0300

    24.2 upd

--- 24.2.py
+++ 24.2.py
@@ -1,29 +1,8 @@
-# word_upper_list = []
-# word_list = []
-# word_code = []
-# while True:
-#     word = input()
-#     if word == '':
-#         break
-#     word_list.append(word)
-#     word = word.upper()
-#     code = [ord(word[i]) - ord('A') + 1 for i in range(len(word))]
-#     word_code.append(sum(code))
-#     # word_upper_list.append(word)
-# word_dict = dict(zip(word_code, word_list))
-# list_keys = list(word_dict.keys())
-# list_keys.sort()
-# for i in list_keys:
-#     print(word_dict[i])
-# # print(*word_dict)
-
-
 word_list = []
 while True:
     word = input()
     if word == '':
         break
     word_list.append(word)
-criteriy = [[ord(word[i]) - ord('A') + 1 for i in range(len(word))] for word in map(str.upper, word_list)]
-sorted(word_list, key = criteriy)
-print(*word_list)
+a = sorted(word_list, key=lambda x: sum([ord(i) - ord('A') + 1 for i in x.upper()]))
+print(*a)
